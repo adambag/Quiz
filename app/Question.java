b997bbb457cbedc9266b07bb54c5ae1501733289
abagi <abaginski2001@gmail.com>
2023-10-19 01:25:24 +0200

    echo "# Quiz" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/adambag/Quiz.git git push -u origin main

---
+++ app/Question.java
@@ -0,0 +1,8 @@
+public class Question {
+    private int questionId;
+    private  boolean trueAnswer;
+    public  Question(int questionId, boolean  trueAnswer){
+        this.questionId=questionId;
+        this.trueAnswer=trueAnswer;
+    }
+}
